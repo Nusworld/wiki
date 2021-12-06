90bd382a71bd825d627c70be01a550c2c4b2dc9b
Nusworld <liuzehao3@outlook.com>
2021-12-07 03:03:49 +0800

    spring boot main starter test

---
+++ src/test/java/cn/edu/ncu/erlys/cn/wiki/WikiApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.ncu.erlys.cn.wiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
