665ad4fe1904a71c0318af7b6ad68c87e16a290a
Nusworld <liuzehao3@outlook.com>
2021-12-07 05:58:18 +0800

    change ebook to ebookResp

--- src/main/java/cn/edu/ncu/erlys/liu/controller/EbookController.java
+++ src/main/java/cn/edu/ncu/erlys/liu/controller/EbookController.java
@@ -1,7 +1,8 @@
 package cn.edu.ncu.erlys.liu.controller;
 
-import cn.edu.ncu.erlys.liu.pojo.Ebook;
+import cn.edu.ncu.erlys.liu.req.EbookReq;
 import cn.edu.ncu.erlys.liu.resp.CommonResp;
+import cn.edu.ncu.erlys.liu.resp.EbookResp;
 import cn.edu.ncu.erlys.liu.service.EbookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,9 +19,9 @@ public class EbookController {
     private EbookService ebookService;
 
     @GetMapping("/list")
-    public CommonResp list() {
-        CommonResp<List<Ebook>> resp = new CommonResp<>();
-        List<Ebook> list = ebookService.list();
+    public CommonResp list(EbookReq req) {
+        CommonResp<List<EbookResp>> resp = new CommonResp<>();
+        List<EbookResp> list = ebookService.list(req);
         resp.setData(list);
         return resp;
     }
