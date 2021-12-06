03d04776fe115548ca11f1672cd0e4966c38503c
Nusworld <liuzehao3@outlook.com>
2021-12-07 05:58:56 +0800

    change ebook service

--- src/main/java/cn/edu/ncu/erlys/liu/service/EbookService.java
+++ src/main/java/cn/edu/ncu/erlys/liu/service/EbookService.java
@@ -2,10 +2,15 @@ package cn.edu.ncu.erlys.liu.service;
 
 import cn.edu.ncu.erlys.liu.mapper.EbookMapper;
 import cn.edu.ncu.erlys.liu.pojo.Ebook;
+import cn.edu.ncu.erlys.liu.pojo.EbookExample;
+import cn.edu.ncu.erlys.liu.req.EbookReq;
+import cn.edu.ncu.erlys.liu.resp.EbookResp;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class EbookService {
@@ -13,8 +18,20 @@ public class EbookService {
     @Autowired
     private EbookMapper ebookMapper;
 
-    public List<Ebook> list() {
-        return ebookMapper.selectByExample(null);
+    public List<EbookResp> list(EbookReq req) {
+        EbookExample ebookExample = new EbookExample();
+        EbookExample.Criteria criteria = ebookExample.createCriteria();
+        criteria.andNameLike("%" + req.getName() + "%");
+        List<Ebook> ebooksList = ebookMapper.selectByExample(ebookExample);
+
+        return ebooksList
+                .stream()
+                .map((e)->{
+                    EbookResp resp = new EbookResp();
+                    BeanUtils.copyProperties(e, resp);
+                    return resp;
+                })
+                .collect(Collectors.toList());
     }
 
 }
