dfb2475f2e71f1aa07ae8a4cb6fa58caadf46cea
Nusworld <liuzehao3@outlook.com>
2021-12-07 03:34:43 +0800

    change directory of application class

--- src/main/java/cn/edu/ncu/erlys/liu/config/WikiApplication.java
+++ src/main/java/cn/edu/ncu/erlys/liu/config/WikiApplication.java
@@ -1,11 +1,13 @@
-package cn.edu.ncu.erlys.cn.wiki;
+package cn.edu.ncu.erlys.liu.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 
+@ComponentScan("cn.edu.ncu.erlys.liu")
 @SpringBootApplication
 public class WikiApplication {
 
