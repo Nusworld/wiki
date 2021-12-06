aa13ec30fe2cc5d1bb67cb993028ebdf38b913f9
Nusworld <liuzehao3@outlook.com>
2021-12-07 06:03:58 +0800

    use copy util to simplify code

--- src/main/java/cn/edu/ncu/erlys/liu/service/EbookService.java
+++ src/main/java/cn/edu/ncu/erlys/liu/service/EbookService.java
@@ -5,12 +5,11 @@ import cn.edu.ncu.erlys.liu.pojo.Ebook;
 import cn.edu.ncu.erlys.liu.pojo.EbookExample;
 import cn.edu.ncu.erlys.liu.req.EbookReq;
 import cn.edu.ncu.erlys.liu.resp.EbookResp;
-import org.springframework.beans.BeanUtils;
+import cn.edu.ncu.erlys.liu.util.CopyUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 public class EbookService {
@@ -24,14 +23,15 @@ public class EbookService {
         criteria.andNameLike("%" + req.getName() + "%");
         List<Ebook> ebooksList = ebookMapper.selectByExample(ebookExample);
 
-        return ebooksList
-                .stream()
-                .map((e)->{
-                    EbookResp resp = new EbookResp();
-                    BeanUtils.copyProperties(e, resp);
-                    return resp;
-                })
-                .collect(Collectors.toList());
+//        return ebooksList
+//                .stream()
+//                .map((e)->{
+//                    EbookResp resp = new EbookResp();
+//                    BeanUtils.copyProperties(e, resp);
+//                    return resp;
+//                })
+//                .collect(Collectors.toList());
+        return CopyUtil.copyList(ebooksList, EbookResp.class);
     }
 
 }
