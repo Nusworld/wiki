63639cc4d657224bf79499f463abb4cbca9ed845
Nusworld <liuzehao3@outlook.com>
2021-12-07 05:40:20 +0800

    ebook controller

---
+++ src/main/java/cn/edu/ncu/erlys/liu/controller/EbookController.java
@@ -0,0 +1,28 @@
+package cn.edu.ncu.erlys.liu.controller;
+
+import cn.edu.ncu.erlys.liu.pojo.Ebook;
+import cn.edu.ncu.erlys.liu.resp.CommonResp;
+import cn.edu.ncu.erlys.liu.service.EbookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/ebook")
+public class EbookController {
+
+    @Autowired
+    private EbookService ebookService;
+
+    @GetMapping("/list")
+    public CommonResp list() {
+        CommonResp<List<Ebook>> resp = new CommonResp<>();
+        List<Ebook> list = ebookService.list();
+        resp.setData(list);
+        return resp;
+    }
+
+}
