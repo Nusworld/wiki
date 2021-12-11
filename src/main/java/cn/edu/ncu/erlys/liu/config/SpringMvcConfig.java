ecb43063733443e8a220e8c155e14581af801982
niT-Tin <1437070389@qq.com>
2021-12-12 00:59:31 +0800

    add interceptor configuration

---
+++ src/main/java/cn/edu/ncu/erlys/liu/config/SpringMvcConfig.java
@@ -0,0 +1,22 @@
+package cn.edu.ncu.erlys.liu.config;
+
+import cn.edu.ncu.erlys.liu.interceptor.LogInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class SpringMvcConfig implements WebMvcConfigurer {
+
+    @Autowired
+    LogInterceptor logInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(logInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/login");
+    }
+
+}
