a48f40eff02db18806778d7e8b488ec8ec69cad8
niT-Tin <1437070389@qq.com>
2021-12-11 16:53:05 +0800

    footer component

---
+++ web/src/components/the-footer.vue
@@ -0,0 +1,18 @@
+<template>
+  <a-layout-footer style="text-align: center">
+    Lzh的电子书 Designed by Liu in 2021 12
+  </a-layout-footer>
+</template>
+
+
+<script lang="ts">
+import {defineComponent} from 'vue';
+
+export default defineComponent({
+  name: 'the-footer',
+});
+</script>
+
+<style scoped>
+
+</style>
