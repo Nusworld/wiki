8820f688fd14d2077d03b1ad5dbbb54e21a015ab
niT-Tin <1437070389@qq.com>
2021-12-12 09:27:49 +0800

    add pagehelper

--- src/main/java/cn/edu/ncu/erlys/liu/service/EbookService.java
+++ src/main/java/cn/edu/ncu/erlys/liu/service/EbookService.java
@@ -6,6 +6,7 @@ import cn.edu.ncu.erlys.liu.pojo.EbookExample;
 import cn.edu.ncu.erlys.liu.req.EbookReq;
 import cn.edu.ncu.erlys.liu.resp.EbookResp;
 import cn.edu.ncu.erlys.liu.util.CopyUtil;
+import com.github.pagehelper.PageHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
@@ -19,6 +20,7 @@ public class EbookService {
     private EbookMapper ebookMapper;
 
     public List<EbookResp> list(EbookReq req) {
+        PageHelper.startPage(1, 3);
         EbookExample ebookExample = new EbookExample();
         EbookExample.Criteria criteria = ebookExample.createCriteria();
         if (!ObjectUtils.isEmpty(req.getName())) {
