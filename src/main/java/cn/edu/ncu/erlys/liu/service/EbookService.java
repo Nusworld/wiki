6cce7f7ffe9757c4f8c33a9cbf73096e2a188046
niT-Tin <1437070389@qq.com>
2021-12-12 09:36:41 +0800

    add pagehelper

--- src/main/java/cn/edu/ncu/erlys/liu/service/EbookService.java
+++ src/main/java/cn/edu/ncu/erlys/liu/service/EbookService.java
@@ -7,6 +7,9 @@ import cn.edu.ncu.erlys.liu.req.EbookReq;
 import cn.edu.ncu.erlys.liu.resp.EbookResp;
 import cn.edu.ncu.erlys.liu.util.CopyUtil;
 import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
@@ -19,15 +22,19 @@ public class EbookService {
     @Autowired
     private EbookMapper ebookMapper;
 
+    private static final Logger LOG = LoggerFactory.getLogger(EbookService.class);
+
     public List<EbookResp> list(EbookReq req) {
-        PageHelper.startPage(1, 3);
         EbookExample ebookExample = new EbookExample();
         EbookExample.Criteria criteria = ebookExample.createCriteria();
         if (!ObjectUtils.isEmpty(req.getName())) {
             criteria.andNameLike("%" + req.getName() + "%");
         }
+        PageHelper.startPage(1, 3);
         List<Ebook> ebooksList = ebookMapper.selectByExample(ebookExample);
-
+        PageInfo<Ebook> pageInfo = new PageInfo<>(ebooksList);
+        LOG.info("总行数： {}", pageInfo.getTotal());
+        LOG.info("总页数: {}", pageInfo.getPages());
 //        return ebooksList
 //                .stream()
 //                .map((e)->{
